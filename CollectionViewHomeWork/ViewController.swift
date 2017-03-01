3a54e22aaf3149ee2677ec8fb8a175df5c8c3fc0
shadowjilu <shadowjilu@gmail.com>
2017-03-01 22:27:56 +0800

    no message

--- CollectionViewHomeWork/ViewController.swift
+++ CollectionViewHomeWork/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+import Photos
+
 final class ViewController: UICollectionViewController {
     
     // MARK: - Properties
@@ -15,26 +17,46 @@ final class ViewController: UICollectionViewController {
     
     fileprivate let itemPerRow: CGFloat = 3
     
-    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 10.0, bottom: 50.0, right: 10.0)
     
     var images =  [ ["1", "2", "3", "4"], ["5", "6"] ]
     
+    var assets = [PHAsset]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let status = PHPhotoLibrary.authorizationStatus()
+        if status == .authorized {
+            let result: PHFetchResult = PHAsset.fetchAssets(with: nil)
+            result.enumerateObjects({ (asset,_,_) in self.assets.append(asset) })
+        }else if status == .notDetermined{
+            PHPhotoLibrary.requestAuthorization( { (status) in
+                DispatchQueue.main.async {
+                    if status == .authorized{
+                        let result: PHFetchResult<PHAsset> = PHAsset.fetchAssets(with: nil)
+                        result.enumerateObjects( { (asset,_,_) in self.assets.append(asset)} )
+                        self.collectionView?.reloadData()
+                    }
+                }
+            } )
+        }
+//        let options = PHFetchOptions()
+//        PHAsset.fetchAssets(with: options)
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        return self.images.count
-    }
+//    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+//        return self.images.count
+//    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO:
-        return images.count
+        return self.assets.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -42,8 +64,10 @@ final class ViewController: UICollectionViewController {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
                                                       for: indexPath) as!PhotoCell
         cell.backgroundColor = UIColor.black
+        let asset = self.assets[indexPath.row]
+        PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: 40, height: 40), contentMode: .aspectFit, options: nil, resultHandler: { (image, nil) in cell.imageView.image = image })
         // Configure the cell
-        cell.imageView.image = UIImage(named: self.images[indexPath.section][indexPath.row])
+//        cell.imageView.image = UIImage(named: self.images[indexPath.section][indexPath.row])
         print(indexPath)
         return cell
         
@@ -54,8 +78,7 @@ extension ViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let paddingSpace = sectionInsets.left * (itemPerRow + 1)
         let availableWidth = view.frame.width - paddingSpace
-        let widthPerItem = availableWidth / itemPerRow
-        
+        let widthPerItem = (availableWidth / itemPerRow - 0.5)
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
     
@@ -65,5 +88,8 @@ extension ViewController: UICollectionViewDelegateFlowLayout{
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
+        
     }
+    
+    
 }
